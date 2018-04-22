5a5955db13505854f76274441837d11e66a92d90
katso <geo.katsogiannis@gmail.com>
2018-04-22 17:10:55 +0300

    Add script to get statistics from workers' logs

---
+++ statistics.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+files=./logs/*.log
+# echo $files;
+
+searchCount=$(cat $files | awk -F ' : ' '{if($2=="search") print $2" "$3}' | wc -l)
+echo "The workers performed a total of $searchCount searches";
+
+mostFound=$(cat $files | awk -F ' : ' '{for(i=4;i<=NF;i++) print $3}' | sort | uniq -c | sort -nr | head -n 1)
+mostFoundWord=$(echo $mostFound | awk -F ' ' '{print $2}')
+filesFound=$(cat $files | awk -F ' : ' -v word=$mostFoundWord '{for(i=4;i<=NF;i++) if($3 == word) print $i}' | sort | uniq -c | wc -l)
+echo "The word $mostFoundWord was found the most times [totalNumFilesFound: $filesFound]"
+
+leastFound=$(cat $files | awk -F ' : ' '{for(i=4;i<=NF;i++) print $3}' | sort | uniq -c | sort -n | head -n 1)
+leastFoundWord=$(echo $leastFound | awk -F ' ' '{print $2}')
+filesFound=$(cat $files | awk -F ' : ' -v word=$leastFoundWord '{for(i=4;i<=NF;i++) if($3 == word) print $i}' | sort | uniq -c | wc -l)
+echo "The word $leastFoundWord was found the least times [totalNumFilesFound: $filesFound]"
